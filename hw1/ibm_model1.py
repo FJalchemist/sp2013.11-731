3cda5383099c11b0b1312d2a701c046e046ae6c6
yibinl <lyyb46@gmail.com>
2013-02-10 22:33:57 -0500

    writing check convergence function

--- hw1/ibm_model1.py
+++ hw1/ibm_model1.py
@@ -10,6 +10,10 @@ returns true if it does.
 def checkConvergence(t_now, t_old):
     THRESHOLD = 0.01;
     if len(t_old) != len(t_now):
+        #Second iteration
+        return False
+    if len(t_now) = 0: 
+        #First iteration
         return False
     sumsq = 0.0;
     for key in t_now:
@@ -31,18 +35,35 @@ INIT_TRANS_PROB = 0.01
 GIVEN = "->"
 
 bitext = [[sentence.strip().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:opts.num_sents]
-f_count = defaultdict(int)
-e_count = defaultdict(int)
-fe_count = defaultdict(int)
 cnt = 0
-while  
-"""
-In the following, n is the sentence order number (id, int), f is the German sentence (list of string), and e is the English sentence (list of string).
-"""
+t_now = defaultdict(float)
+t_old = defaultdict(float)
+set_e = set()
+set_f = set()
+while not checkConvergence(t_now, t_old):
+    count = defaultdict(float)
+    total = defaultdict(float)
+    """
+    In the following, n is the sentence order number (id, int), f is the German sentence (list of string), and e is the English sentence (list of string).
+    """
     for (n, (f, e)) in enumerate(bitext):
-        print n
-        print f
-        print e
+        set_e.update(set(e))
+        set_f.update(set(f))
+
+        s-total = defaultdict(float)
+        for eword in e:
+            for fword in f:
+                if t_now[e] != 0:
+                    s-total[e] += t_now[e + GIVEN + f]
+                else:
+                    t_now[e + GIVEN + f] = INIT_TRANS_PROB
+                    s-total(e) += t_now[e + GIVEN + f]
+        for eword in e:
+            for fword in f:
+                count[e + GIVEN + f] += t_now[e + GIVEN + f] / s-total[e]
+                total[f] += t_now[e + GIVEN + f] / s-total[e]
+    for 
+
     #  for f_i in set(f):
     #    f_count[f_i] += 1
     #    for e_j in set(e):
