dafaabd0e9b724aebb8ee6cfa4d18fe351cb1aab
yibinl <lyyb46@gmail.com>
2013-02-11 15:00:34 -0500

    finished writing ibm model 1 code, debugging...

--- hw1/ibm_model1.py
+++ hw1/ibm_model1.py
@@ -12,7 +12,7 @@ def checkConvergence(t_now, t_old):
     if len(t_old) != len(t_now):
         #Second iteration
         return False
-    if len(t_now) = 0: 
+    if len(t_now) == 0: 
         #First iteration
         return False
     sumsq = 0.0;
@@ -33,10 +33,20 @@ def simpleAlignmentEst(t_prob, e, f):
         for j in range(len(f)):
             fword = f[j]
             if t_prob[eword + GIVEN + fword] > best_align[0]:
-                best_align[0] = t_prob[eword + GIVEN + fword]
-                best_align[1] = i
-        alignment.append((j,i))
-    return alignment
+                best_align = (t_prob[eword + GIVEN + fword], j)
+        alignment.append((best_align[1],i))
+    return sorted(alignment)
+
+def printBestAlignments(t_prob, bitext):
+    """
+    Print all best alignments in the training data
+    """
+    for (n, (f, e)) in enumerate(bitext):
+        alignment = simpleAlignmentEst(t_prob, e, f)
+        print alignment
+#        for (f_i, e_i) in alignment:
+#           sys.stdout.write("%i-%i" % (f_i, e_i))
+#        sys.stdout.write("\n")
 
 optparser = optparse.OptionParser()
 optparser.add_option("-b", "--bitext", dest="bitext", default="data/dev-test-train.de-en", help="Parallel corpus (default data/dev-test-train.de-en)")
@@ -55,6 +65,9 @@ t_old = defaultdict(float)
 set_e = set()
 set_f = set()
 while not checkConvergence(t_now, t_old):
+    cnt += 1
+    sys.stderr.write("%i-th iteration.\n" % cnt)
+
     count = defaultdict(float)
     total = defaultdict(float)
     """
@@ -65,33 +78,25 @@ while not checkConvergence(t_now, t_old):
         set_e.update(set(e))
         set_f.update(set(f))
 
-        s-total = defaultdict(float)
+        s_total = defaultdict(float)
         for eword in e:
             for fword in f:
                 if t_now[eword] != 0:
-                    s-total[eword] += t_now[eword + GIVEN + fword]
+                    s_total[eword] += t_now[eword + GIVEN + fword]
                 else:
                     t_now[eword + GIVEN + fword] = INIT_TRANS_PROB
-                    s-total(eword) += t_now[eword + GIVEN + fword]
+                    s_total[eword] += t_now[eword + GIVEN + fword]
         #E
         for eword in e:
             for fword in f:
-                count[eword + GIVEN + fword] += t_now[eword + GIVEN + fword] / s-total[eword]
-                total[fword] += t_now[eword + GIVEN + fword] / s-total[eword]
+                count[eword + GIVEN + fword] += t_now[eword + GIVEN + fword] / s_total[eword]
+                total[fword] += t_now[eword + GIVEN + fword] / s_total[eword]
     t_old = t_now
     #M
     for eword in set_e:
         for fword in set_f:
             t_now[eword + GIVEN + fword] = count[eword + GIVEN + fword] / total[fword]
 
-
-    #  for f_i in set(f):
-    #    f_count[f_i] += 1
-    #    for e_j in set(e):
-    #      fe_count[(f_i,e_j)] += 1
-    #  for e_j in set(e):
-    #    e_count[e_j] += 1
-    #  if n % 500 == 0:
-    #    sys.stderr.write(".")
+printBestAlignments(t_now, bitext)
 
 
