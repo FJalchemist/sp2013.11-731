89708eb4520f92a7551677f116fb56f4a5d97437
yibinl <lyyb46@gmail.com>
2013-02-11 21:21:08 -0500

    made some small changes to save memory.. memorygit add ibm_model1.py! gosh

--- hw1/ibm_model1.py
+++ hw1/ibm_model1.py
@@ -8,16 +8,15 @@ def checkConvergence(t_now, t_old):
     check wether the translation probability is converged.
     returns true if it does.
     """
-    THRESHOLD = 0.01;
-    if len(t_old) != len(t_now):
-        #Second iteration
-        return False
+    THRESHOLD = 0.15;
     if len(t_now) == 0: 
         #First iteration
         return False
     sumsq = 0.0;
     for key in t_now:
+#        sys.stderr.write("%s, %f, " % (key, (t_now[key] - t_old[key])**2))
         sumsq += (t_now[key] - t_old[key])**2
+    sys.stderr.write("sumsq: %f.\n" % sumsq)
     if THRESHOLD > sumsq:
         return True
     else:
@@ -29,11 +28,12 @@ def simpleAlignmentEst(t_prob, e, f):
     """
     alignment = []
     for i in range(len(e)):
+        eword = e[i]
         best_align = (-1.0, -1)
         for j in range(len(f)):
             fword = f[j]
-            if t_prob[eword + GIVEN + fword] > best_align[0]:
-                best_align = (t_prob[eword + GIVEN + fword], j)
+            if t_prob[eword , fword] > best_align[0]:
+                best_align = (t_prob[eword , fword], j)
         alignment.append((best_align[1],i))
     return sorted(alignment)
 
@@ -43,10 +43,10 @@ def printBestAlignments(t_prob, bitext):
     """
     for (n, (f, e)) in enumerate(bitext):
         alignment = simpleAlignmentEst(t_prob, e, f)
-        print alignment
-#        for (f_i, e_i) in alignment:
-#           sys.stdout.write("%i-%i" % (f_i, e_i))
-#        sys.stdout.write("\n")
+#        print alignment
+        for (f_i, e_i) in alignment:
+           sys.stdout.write("%i-%i " % (f_i, e_i))
+        sys.stdout.write("\n")
 
 optparser = optparse.OptionParser()
 optparser.add_option("-b", "--bitext", dest="bitext", default="data/dev-test-train.de-en", help="Parallel corpus (default data/dev-test-train.de-en)")
@@ -56,46 +56,64 @@ optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxi
 sys.stderr.write("Training IBM model 1...\n")
 
 INIT_TRANS_PROB = 0.01
-GIVEN = "->"
+#GIVEN = ">"
 
 bitext = [[sentence.strip().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:opts.num_sents]
 cnt = 0
-t_now = defaultdict(float)
-t_old = defaultdict(float)
-set_e = set()
-set_f = set()
-while not checkConvergence(t_now, t_old):
+t_now = {}
+t_old = None
+#set_e = set()
+#set_f = set()
+#while (not checkConvergence(t_now, t_old)) && cnt < 10:
+while cnt < 10:
     cnt += 1
-    sys.stderr.write("%i-th iteration.\n" % cnt)
+    sys.stderr.write("\n%i-th iteration." % cnt)
 
-    count = defaultdict(float)
-    total = defaultdict(float)
+    count = {}
+    total = {}
     """
     In the following, n is the sentence order number (id, int), f is the German sentence (list of string), and e is the English sentence (list of string).
     """
     for (n, (f, e)) in enumerate(bitext):
-        #get all unique tokens in e and f.
-        set_e.update(set(e))
-        set_f.update(set(f))
+#        if cnt == 1:
+#            #get all unique tokens in e and f.
+#            set_e.update(set(e))
+#            set_f.update(set(f))
 
         s_total = defaultdict(float)
         for eword in e:
             for fword in f:
-                if t_now[eword] != 0:
-                    s_total[eword] += t_now[eword + GIVEN + fword]
+                if t_now.get((eword, fword), 0.0) != 0.0:
+                    s_total[eword] += t_now[eword, fword]
                 else:
-                    t_now[eword + GIVEN + fword] = INIT_TRANS_PROB
-                    s_total[eword] += t_now[eword + GIVEN + fword]
+                    t_now[eword, fword] = INIT_TRANS_PROB
+                    s_total[eword] += t_now[eword, fword]
+#        sys.stderr.write("s_total size: %i" % (len(s_total)))
         #E
         for eword in e:
             for fword in f:
-                count[eword + GIVEN + fword] += t_now[eword + GIVEN + fword] / s_total[eword]
-                total[fword] += t_now[eword + GIVEN + fword] / s_total[eword]
-    t_old = t_now
+                if count.get((eword, fword), 0.0) == 0.0:
+                    count[eword, fword] = t_now[eword, fword] / s_total[eword]
+                else:
+                    count[eword, fword] += t_now[eword, fword] / s_total[eword]
+                if total.get(fword, 0.0) == 0.0:
+                    total[fword] = t_now[eword, fword] / s_total[eword]
+                else:
+                    total[fword] += t_now[eword, fword] / s_total[eword]
+
+        if n % 1000 == 0:
+            sys.stderr.write(".")
+#    t_old = t_now
+    t_now = {}
     #M
-    for eword in set_e:
-        for fword in set_f:
-            t_now[eword + GIVEN + fword] = count[eword + GIVEN + fword] / total[fword]
+    if cnt == 1:
+        sys.stderr.write("count size: %i.\n" % len(count))
+    for key in count:
+        eword = key[0]
+        fword = key[1]
+#        sys.stderr.write("fword: %s.\n" % fword)
+        if total.get(fword, 0.0) != 0.0:
+            t_now[key] = count[key] / total[fword]
 
 printBestAlignments(t_now, bitext)
 
