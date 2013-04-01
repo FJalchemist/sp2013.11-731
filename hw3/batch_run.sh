e4bd867fa05d442ae738ff1c9e3227d3d71ca3fb
yibinl <yibinl@thinkpad.(none)>
2013-04-01 17:19:15 -0400

    changed alpha, now it finally works

--- hw3/batch_run.sh
+++ hw3/batch_run.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
 
-./decode_mod.py -s 30 -a 0.3 | ./grade
-./decode_mod.py -s 30 -a 0.25 | ./grade
-./decode_mod.py -s 30 -a 0.2 | ./grade
-./decode_mod.py -s 30 -a 0.15 | ./grade
-./decode_mod.py -s 30 -a 0.1 | ./grade
-./decode_mod.py -s 30 -a 0.05 | ./grade
+./decode_mod.py -s 10000 -a 0.95 -i data/21.txt >> result21
+./decode_mod.py -s 500 -a 0.95 -i data/22.txt >> result22
+./decode_mod.py -s 500 -a 0.95 -i data/23.txt >> result23
+./decode_mod.py -s 500 -a 0.95 -i data/24.txt >> result24
+./decode_mod.py -s 500 -a 0.95 -i data/25.txt >> result25
+./decode_mod.py -s 10000 -a 0.95 -i data/26.txt >> result26
+./decode_mod.py -s 500 -a 0.95 -i data/27.txt >> result27
+./decode_mod.py -s 5000 -a 0.95 -i data/28.txt >> result28
+./decode_mod.py -s 500 -a 0.95 -i data/29.txt >> result29
+./decode_mod.py -s 500 -a 0.95 -i data/30.txt >> result30
+./decode_mod.py -s 500 -a 0.95 -i data/31.txt >> result31
+./decode_mod.py -s 500 -a 0.95 -i data/32.txt >> result32
+./decode_mod.py -s 500 -a 0.95 -i data/33.txt >> result33
+./decode_mod.py -s 10000 -a 0.95 -i data/34.txt >> result34
+./decode_mod.py -s 500 -a 0.95 -i data/35.txt >> result35
+./decode_mod.py -s 500 -a 0.95 -i data/36.txt >> result36
+./decode_mod.py -s 500 -a 0.95 -i data/37.txt >> result37
+./decode_mod.py -s 500 -a 0.95 -i data/38.txt >> result38
+./decode_mod.py -s 500 -a 0.95 -i data/39.txt >> result39
+./decode_mod.py -s 10000 -a 0.95 -i data/40.txt >> result40
+./decode_mod.py -s 500 -a 0.95 -i data/41.txt >> result41
+./decode_mod.py -s 5000 -a 0.95 -i data/42.txt >> result42
+./decode_mod.py -s 500 -a 0.95 -i data/43.txt >> result43
+./decode_mod.py -s 500 -a 0.95 -i data/44.txt >> result44
+./decode_mod.py -s 5000 -a 0.95 -i data/45.txt >> result45
+./decode_mod.py -s 500 -a 0.95 -i data/46.txt >> result46
+./decode_mod.py -s 10000 -a 0.95 -i data/47.txt >> result47
+./decode_mod.py -s 500 -a 0.95 -i data/48.txt >> result48
+./decode_mod.py -s 500 -a 0.95 -i data/49.txt >> result49
+./decode_mod.py -s 500 -a 0.95 -i data/50.txt >> result50
+./decode_mod.py -s 500 -a 0.95 -i data/51.txt >> result51
+./decode_mod.py -s 10000 -a 0.95 -i data/52.txt >> result52
+./decode_mod.py -s 500 -a 0.95 -i data/53.txt >> result53
+./decode_mod.py -s 500 -a 0.95 -i data/54.txt >> result54
+./decode_mod.py -s 10000 -a 0.95 -i data/55.txt >> result55
